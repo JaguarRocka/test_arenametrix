4ee7882b3f4185a2b61271e134e441c4a957da8b
jean lafore <jean.l94240@gmail.com>
2025-05-23 23:20:52 +0200

    add customers link to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   has_many :representations, dependent: :destroy
   has_many :reservations, through: :representations
   has_many :tickets, through: :reservations
+  has_many :customers, through: :reservations
 
   validates :name, presence: true
 
