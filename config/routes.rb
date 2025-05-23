c888814aff9b7d6e4b1251a562cbb5c6128fe8ae
jean lafore <jean.l94240@gmail.com>
2025-05-23 13:04:43 +0200

    add upload page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "csv_import#upload"
+  post "csv_import/import", to: "csv_import#import", as: :import_csv
 
   get "up" => "rails/health#show", as: :rails_health_check
 
