f04afad0ba933af76dc109a8f1487bd3ed02d667
jean lafore <jean.l94240@gmail.com>
2025-05-24 09:44:44 +0200

    fix routins

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root "csv_import#upload"
-  post "csv_import/import", to: "csv_import#import", as: :import_csv
-
+  root "dashboard#home"
   get 'event_value', to: 'dashboard#event_value', as: :event_value
-  get 'dashboard', to: 'dashboard#home'
+
+  get 'import_csv', to: 'csv_import#upload', as: :upload_csv
+  post "/import", to: "csv_import#import", as: :import_csv
+
 
   get "up" => "rails/health#show", as: :rails_health_check
 
